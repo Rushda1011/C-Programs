109bc385329371da0976d46b5d2bb05d9993553d
Rushda1011 <rushdaraheem2005@gmail.com>
2026-04-18 12:58:34 +0530

    Create Prime.c

---
+++ Prime.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int n,i,flag=1;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+        if(n%i==0) flag=0;
+    if(flag) printf("Prime");
+    else printf("Not Prime");
+}
